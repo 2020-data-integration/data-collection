e61c3618a2fd499e8a3232ea84385b864b632772
赵文祺 <1027572886a@gmail.com>
2020-11-14 19:33:41 +0800

    完成知识图谱

--- import.sh
+++ import.sh
@@ -1,4 +1,4 @@
-neo4j-admin import \
+./neo4j-admin import \
     --id-type=STRING \
     --skip-duplicate-nodes=true \
     --ignore-empty-strings=true \
@@ -6,4 +6,4 @@ neo4j-admin import \
     --nodes=new_import/industry.csv \
     --nodes=new_import/holder.csv \
     --relationships=new_import/industry_company.csv \
-    --relationships=new_import/holder_company.csv \
+    --relationships=new_import/holder_company.csv
