bb6727d2df01b452e2a0777f2a9dd35a2c404124
赵文祺 <1027572886a@gmail.com>
2020-11-20 21:30:18 +0800

    新建new数据库

--- import.sh
+++ import.sh
@@ -4,6 +4,5 @@
     --ignore-empty-strings=true \
     --nodes=new_import/company.csv \
     --nodes=new_import/industry.csv \
-    --nodes=new_import/holder.csv \
     --relationships=new_import/industry_company.csv \
-    --relationships=new_import/holder_company.csv
+    --relationships=new_import/company_company.csv
